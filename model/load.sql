ec8119b8d87f4ee877bfa2768e558640c2b63c7d
Carson Tang <tang.carson@gmail.com>
2017-05-10 18:41:40 -0700

    uncomment out loading of orders relation

--- model/load.sql
+++ model/load.sql
@@ -1,8 +1,7 @@
-/*COPY orders(order_id, user_id, order_eval_set, order_number, order_dow, order_hour_of_day, days_since_prior)
+COPY orders(order_id, user_id, order_eval_set, order_number, order_dow, order_hour_of_day, days_since_prior)
 FROM '/home/carson/Documents/instacart_grocery_2017_analysis/data/instacart_2017_05_01/orders.csv'
 DELIMITER ','
 CSV HEADER;
-*/
 
 COPY departments(department_id, department)
 FROM '/home/carson/Documents/instacart_grocery_2017_analysis/data/instacart_2017_05_01/departments.csv'
