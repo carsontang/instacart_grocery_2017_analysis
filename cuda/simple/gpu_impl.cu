df1087769b9e8cde3b45fe6a2ad995d6546fa8e6
Carson Tang <ctang@fb.com>
2017-05-18 14:52:34 +0800

    implemented kernel for add

--- cuda/simple/gpu_impl.cu
+++ cuda/simple/gpu_impl.cu
@@ -1,5 +1,28 @@
 #include "pybind_simple.h"
 
+__global__ void kernel_add(int *i, int *j, int *result) {
+    *result = *i + *j;
+}
+
 int add(int i, int j) {
-    return i + j;
+    size_t nBytes = sizeof(i);
+    int result = 0;
+
+    int *d_i, *d_j, *d_result;
+    cudaMalloc( (int **)&d_i, nBytes);
+    cudaMalloc( (int **)&d_j, nBytes);
+    cudaMalloc( (int **)&d_result, nBytes);
+
+    cudaMemcpy(d_i, &i, nBytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_j, &j, nBytes, cudaMemcpyHostToDevice);
+
+    kernel_add<<<1, 1>>>(d_i, d_j, d_result);
+
+    cudaMemcpy(&result, d_result, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_i);
+    cudaFree(d_j);
+    cudaFree(d_result);
+
+    return result;
 }
