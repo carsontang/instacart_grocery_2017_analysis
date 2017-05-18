2d6498165cef35fe19eb5a83a12ee00a1265963f
Carson Tang <ctang@fb.com>
2017-05-18 15:28:12 +0800

    implemented script to test implementation of pybind_simple module

---
+++ cuda/simple/test_pybind_simple.py
@@ -0,0 +1,15 @@
+import sys
+
+try:
+    import pybind_simple as C
+except ImportError as e:
+    print "Couldn't import pybind_simple"
+    sys.exit(1)
+
+import numpy as np
+
+a1 = np.array([i for i in range(1000)])
+a2 = np.array([i for i in range(1000)])
+
+result = C.add_arrays(a1, a2)
+print result
